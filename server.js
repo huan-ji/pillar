44a9319386be74fab70da6bc988dc1eb2812d1c8
Huan Ji <hji@credible.com>
2019-03-24 08:53:41 -0700

    Fix server

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.listen(app.get('port'), () => console.log(`Listening on port ${port}`));
+app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
